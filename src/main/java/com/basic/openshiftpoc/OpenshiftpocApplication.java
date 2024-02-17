a26f18ed9c846fd6be138ffd69da11e035a26d2c
Praveen Raju <praveenraju2889@gmail.com>
2024-02-17 12:16:31 +0400

    openshift

---
+++ src/main/java/com/basic/openshiftpoc/OpenshiftpocApplication.java
@@ -0,0 +1,21 @@
+package com.basic.openshiftpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class OpenshiftpocApplication {
+	
+	@GetMapping("/")
+	public String getMessgaString() {
+		return "Welcome openshift!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftpocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basic/openshiftpoc/OpenshiftpocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.openshiftpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenshiftpocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
